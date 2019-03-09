e2561b3fa91efb69b43c445a0b488aedd57d9b9f
errorr404 <dkbishwash@gmail.com>
2019-03-09 15:44:52 +0530

    added route for get_all_channels

--- index.js
+++ index.js
@@ -13,9 +13,24 @@ mongoose.connect('mongodb://localhost:27017/channeldata',({useNewUrlParser:true}
 .then(()=>console.log('connected to mongodb'))
 .catch((err)=>console.log(err))
 
+var count = 0
 jsoncontent.map(content=>{
 var channel = new Channel(content)
 channel.save().then(channel_res=>{
-  console.log('saved')
+  count++
+  console.log(count)
 }).catch(err=>console.log(err))
+},()=>{
+  console.log(count)
 })
+
+const app = express()
+
+app.get('/get_channel',(req,res)=>{
+  Channel.find({})
+        .then(channel=>{
+          res.status(200).send(channel)
+        }).catch(err=>console.log(err))
+})
+
+app.listen(5000,()=>console.log('server started'))
